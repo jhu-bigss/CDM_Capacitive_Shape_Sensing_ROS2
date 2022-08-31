142a831582f08240385413a7901c1d828aed1a0e
WENPENG WANG <wwang153@jhu.edu>
2022-08-31 12:36:32 -0400

    check

---
+++ snake_param_reader/snake_param_reader/param_node.py
@@ -0,0 +1,28 @@
+from os import name
+import rclpy
+from rclpy.node import Node
+
+class TestParams(Node):
+
+    def __init__(self):
+        super().__init__('test_params_rclpy')
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('double_A', 0.0),
+                ('double_B', 0.0),
+                ('double_C', 0.0)
+            ])
+        
+
+# The following is just to start the node
+def main(args=None):
+    rclpy.init(args=args)
+    node = TestParams()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
